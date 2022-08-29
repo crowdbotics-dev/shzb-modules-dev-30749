05310d7e7815242ce1ac4fc40a4a527a92ce49c6
Shahzaib <shahzaibali1022@yahoo.com>
2022-08-29 23:20:10 +0500

    payments modules

---
+++ ios/fix.swift
@@ -0,0 +1,8 @@
+//
+//  fix.swift
+//  shzb_modules_dev_30749
+//
+//  Created by Shahr on 8/29/22.
+//
+
+import Foundation
--- options/options.js
+++ options/options.js
@@ -4,9 +4,9 @@ export const globalOptions = {
   name: "shzb_modules_dev_30749",
   url: "https://shzb_modules_dev_30749.botics.co",
   api: "https://shzb_modules_dev_30749.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
   }
-}
+};
